3a83cec6b066c287e00b243c942d9f333c346759
wenzouquan <359945126@qq.com>
2017-08-17 23:37:40 +0800

    找服务

--- Client.php
+++ Client.php
@@ -1,6 +1,7 @@
 <?php
 namespace phpkit\thriftrpc;
 // 引入客户端文件
+require_once __DIR__ . "/apc.php";
 require_once __DIR__ . "/Thrift/ClassLoader/ThriftClassLoader.php";
 use \Thrift\ClassLoader\ThriftClassLoader;
 use \Thrift\Protocol\TBinaryProtocol;
@@ -14,16 +15,22 @@ class Client {
 			$this->registerDefinition($dirs);
 		}
 	}
-	public function getProtocol($serviceName) {
-		$socket = new TSocket("127.0.0.1", 8091);
+	public function getProtocol($serviceName, $serviceAddress = "", $servicePort = "") {
+		//随机找到一个服务
+		$consul = new \phpkit\consulapi\Consul();
+		$services = $consul->findService($serviceName);
+		$key = array_rand($services, 1);
+		$serviceAddress = $serviceAddress ? $serviceAddress : $services[$key]['ServiceAddress'];
+		$servicePort = $servicePort ? $servicePort : $services[$key]['ServicePort'];
+		$socket = new TSocket($serviceAddress, $servicePort);
 		$transport = new TFramedTransport($socket);
 		$protocol = new TBinaryProtocol($transport);
 		$transport->open();
 		return $protocol;
 	}
 
-	public function getRPCService($serviceName) {
-		$protocol = $this->getProtocol($serviceName);
+	public function getRPCService($serviceName, $serviceAddress = "", $servicePort = "") {
+		$protocol = $this->getProtocol($serviceName, $serviceAddress, $servicePort);
 		$arr = explode("\\", $serviceName);
 		$name = $arr[count($arr) - 1];
 		$tMultiplexedProtocol = new TMultiplexedProtocol($protocol, $serviceName);
---
+++ apc.php
@@ -0,0 +1,23 @@
+<?php
+if (!function_exists('apc_add')) {
+	if (!extension_loaded('apcu')) {
+		throw new \Exception("not  suppert apc", 1);
+	}
+	function apc_add($key = "", $value = "", $exp = 0) {
+		return apcu_add($key, $value, $exp);
+	}
+	function apc_store($key = "", $value = "", $exp = 0) {
+		return apcu_store($key, $value, $exp);
+	}
+	function apc_fetch($key = "", $success = "") {
+		return apcu_fetch($key, $success);
+	}
+	function apc_delete($key = "") {
+		return apcu_delete($key);
+	}
+
+	function apc_cache_info() {
+		return apcu_cache_info();
+	}
+
+}
